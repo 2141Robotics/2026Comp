b3f03c60f6738113d7b9af79a3f1dba0b2245da8
2141Robotics <2141robotics@gmail.com>
2026-01-22 11:42:22 -0800

    Vision

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,18 +1,15 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.MotionMagicDutyCycle;
+import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
     private final TalonFX climberMotor;
-
-    private double desiredClimberHeight;
 
     public Climber() {
         climberMotor = new TalonFX(ClimberConstants.climberMotorPort);
@@ -20,34 +17,30 @@ public class Climber extends SubsystemBase {
     }
 
     private void init() {
-        desiredClimberHeight = 0.0;
         TalonFXConfiguration config = new TalonFXConfiguration();
         config.Slot0.kP = 80;
         config.Slot0.kI = 0;
         config.Slot0.kD = 0;
         config.Slot0.kV = 0;
-        limberMotor.getConfigurator().apply(config);
+        climberMotor.getConfigurator().apply(config);
         climberMotor.setNeutralMode(NeutralModeValue.Brake);
     }
 
     public void moveUp() {
-        setDesiredClimberHeight(desiredClimberHeight + ClimberConstants.CLIMBER_SPEED);
+        // setDesiredClimberHeight(desiredClimberHeight + ClimberConstants.CLIMBER_SPEED);
+        climberMotor.setControl(new DutyCycleOut(-1));
     }
 
     public void moveDown() {
-        setDesiredClimberHeight(desiredClimberHeight - ClimberConstants.CLIMBER_SPEED);
+        // setDesiredClimberHeight(desiredClimberHeight - ClimberConstants.CLIMBER_SPEED);
+        climberMotor.setControl(new DutyCycleOut(1));
     }
 
-    private void setDesiredClimberHeight(double height) {
-        desiredClimberHeight = height;
-    }
+    
 
     @Override
     public void periodic() {
         super.periodic();
-        climberMotor.setControl(new MotionMagicDutyCycle(desiredClimberHeight));
-        
-        //Units are unnecessary to calculate, as the value only matters relative to itself.
-        SmartDashboard.putNumber("Climber Height (units unknown)", desiredClimberHeight);
+        climberMotor.setControl(new DutyCycleOut(0));
     }
 }
